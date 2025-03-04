939c5c17f486368b0f45d58cb7244133ceafbd26
Miroslav Mlynarik <miroslav.mlynarik@gmail.com>
2025-03-04 01:11:21 +0100

    Refactor

--- src/devmons/services.py
+++ src/devmons/services.py
@@ -1,12 +1,12 @@
 from sqlalchemy.orm import Session
 
-from devmons.coingecko import CGCoin, CGCoinCreate, get_coins_data
+from devmons.coingecko import CGCoin, get_coins_data
 from devmons.repository import CGCoinRepository
 
 
-def add_coins(coin: CGCoinCreate, repo: CGCoinRepository, session: Session) -> list[CGCoin]:
-    coins = get_coins_data(coin.symbol)
-    for c in coins:
-        repo.add(c)
+def add_coins(symbol: str, repo: CGCoinRepository, session: Session) -> list[CGCoin]:
+    coins = get_coins_data(symbol)
+    for coin in coins:
+        repo.add(coin)
     session.commit()
     return coins

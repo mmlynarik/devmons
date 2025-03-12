de213e1c348d1499ce7895fdcc55cfa48f0eae58
Miro Mlynarik <44208384+mmlynarik@users.noreply.github.com>
2025-03-12 09:32:05 +0100

    Update dependency.py

--- src/devmons/dependency.py
+++ src/devmons/dependency.py
@@ -2,12 +2,12 @@ from httpx import AsyncClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from devmons.settings import BACKEND_DB_URL
+from devmons.settings import BACKEND_DB_URL, CG_API_URL 
 
 engine = create_engine(BACKEND_DB_URL, echo=False)
 
 Session = sessionmaker(engine)
-HTTPClient = AsyncClient()
+HTTPClient = AsyncClient(base_url=CG_API_URL)
 
 
 def get_db_session():
